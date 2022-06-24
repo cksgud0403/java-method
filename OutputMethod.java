e9326f8850b94516f614cffb3be743fefa068847
cksgud0403 <cksgud0403@naver.com>
2022-06-24 20:59:13 +0900

    Feat: Java WhyMethod (return)

---
+++ OutputMethod.java
@@ -0,0 +1,11 @@
+
+public class OutputMethod {
+public static int a() {
+	return 1;
+}
+	public static void main(String[] args) {
+		System.out.println(a());
+
+	}
+
+}
--- WhyMethod.java
+++ WhyMethod.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WhyMethod {
 	public static void printTwoTimesA(String text, String delimiter) {
@@ -6,12 +8,33 @@ public class WhyMethod {
 		System.out.println(text);
 	}
 	
-	public static void main(String[] args) {
+	public static void writeFileTwoTimes(String text, String delimiter) throws IOException {
+		FileWriter fw = new FileWriter("output.txt");
+		fw.write(delimiter+"\n");
+		fw.write(text+"\n");
+		fw.write(text+"\n");
+		fw.close();
+		
+	}
+	
+	public static String twoTimes(String text, String delimiter) {
+		String out = "";
+		out += delimiter+"\n";
+		out += text + "\n";
+		out += text+"\n";
+		return out;
+	}
+	
+	public static void main(String[] args) throws IOException {
 		//100000000
 		printTwoTimesA("A", "-");
 		//100000000
 		printTwoTimesA("B", "*");
 		//100000000
 		printTwoTimesA("C", "/");
+		
+		FileWriter fw = new FileWriter("output.txt");
+		fw.write(twoTimes("A", "-"));
+		fw.close();
 	}
 }
